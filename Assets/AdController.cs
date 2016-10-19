af200d60396cb80e0f4b765fadb85528f103a210
Huel Fuchsberger <huel.fuchsberger@wooga.com>
2016-10-19 20:54:17 +0200

    Init

---
+++ Assets/AdController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Advertisements;
+
+public class AdController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ShowRewardedAd();
+	}
+
+	public void ShowAd()
+	{
+		if (Advertisement.IsReady())
+		{
+			Advertisement.Show();
+		}
+	}
+		
+	public void ShowRewardedAd()
+	{
+		if (Advertisement.IsReady("rewardedVideoZone"))
+		{
+			var options = new ShowOptions { resultCallback = HandleShowResult };
+			Advertisement.Show("rewardedVideoZone", options);
+		}
+	}
+
+	private void HandleShowResult(ShowResult result)
+	{
+		switch (result)
+		{
+		case ShowResult.Finished:
+			Debug.Log("The ad was successfully shown.");
+				//
+				// YOUR CODE TO REWARD THE GAMER
+				// Give coins etc.
+				break;
+		case ShowResult.Skipped:
+				Debug.Log("The ad was skipped before reaching the end.");
+				break;
+		case ShowResult.Failed:
+				Debug.LogError("The ad failed to be shown.");
+				break;
+		}
+	}
+}
---
+++ Assets/AnalyticsScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+
+public class AnalyticsScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		TestPurchase ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void LevelUp()
+	{
+
+		Analytics.CustomEvent("gameOver", new Dictionary<string, object>
+			{
+				{ "newLevel", 2 },
+				{ "trigger", "purchase" }
+
+			});
+	}
+
+	void TestPurchase()
+	{
+		Analytics.Transaction("purchaseItem1", 1.0m ,"EUR", "null", "null");
+
+		Analytics.SetUserId ("00000000XX");
+		Analytics.SetUserBirthYear (2016);
+		Analytics.SetUserGender (Gender.Unknown);
+	}
+}
